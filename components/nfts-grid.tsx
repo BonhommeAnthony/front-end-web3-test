34f748ce999459b15a5c832352ddd4f7d4d1c768
Anthony Bonhomme <61671988+BonhommeAnthony@users.noreply.github.com>
2023-11-10 15:35:47 +0100

    fix x space on mobile for nft grid

--- components/nfts-grid.tsx
+++ components/nfts-grid.tsx
@@ -8,7 +8,7 @@ export interface INftsGridProps {
 
 export function NftsGrid({ nfts }: INftsGridProps) {
     return (
-        <div className="flex w-full space-y-4 md:space-y-4 space-x-4">
+        <div className="flex w-full space-y-4 md:space-y-4 md:space-x-4">
             <NftsFilter />
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-4 w-full">
                 {nfts.map((nft) => (
